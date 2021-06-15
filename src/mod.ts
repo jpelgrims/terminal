9fdd8a9c5a2429530024763809fc11b6ca8ba250
Jelle Pelgrims <github@jellepelgrims.com>
2021-06-15 22:22:35 +0200

    Add forgotten export statement to mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -3,4 +3,5 @@ export * from "./cell.ts";
 export * from "./image.ts";
 export * from "./terminal.ts";
 export * from "./event.ts";
+export * from "./canvas.ts";
 export * as EscapeSequence from "./escape_sequences.ts";
